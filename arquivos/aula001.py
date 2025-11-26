5ca50c11f7cdca71db9b6b8280279a25ef639a77
Magno V Gomes <magnovgomes74@gmail.com>
2025-11-26 15:47:10 -0300

    Corrigindo conflito no arquivo aula001

--- arquivos/aula001.py
+++ arquivos/aula001.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 """ 
 OBS: Estas primeiras aulas eu já sei todos os comandos, 
 estou apenas revendo o conteúdo.
@@ -6,14 +6,13 @@ estou apenas revendo o conteúdo.
 
 
 # Esta aula foi sobre 'Comentários no python'
-=======
+
 # Esta aula foi foi sobre 'Comentários no vscode'
->>>>>>> 11ddae6 (Ajuste final da estrutura)
+
 
 # Meu primeiro comando, para saber que '#' só comenta uma linha
 print('Deus é fiel!!')
 
-<<<<<<< HEAD
 
 """
 DocString -> Não é um comentário! São guardadas na memória.
@@ -21,9 +20,7 @@ DocString -> Não é um comentário! São guardadas na memória.
 Permite escrever/documentar várias linhas, ou seja, 
 tudo que estiver detro das aspas duplas ou simples,
 desde que sejam triplas acima e abaixo
-=======
-"""
-Permite comentar várias linhas, ou seja, tudo que estiver detro das apas acima e abaixo
->>>>>>> 11ddae6 (Ajuste final da estrutura)
 
+
+Permite comentar várias linhas, ou seja, tudo que estiver detro das apas acima e abaixo
 """
