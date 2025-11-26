5ec69590bea577d39a08ecc9dfe0fa4b4646e2ca
Magno V Gomes <magnovgomes74@gmail.com>
2025-11-26 15:36:25 -0300

    Finalizando aula 003 - Strings introdução aos tipos de dados

---
+++ arquivos/aula003.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(123)
+
+# Aspas simples
+print('\nMagno V Gomes')
+print(1, 'Magno "V Gomes"\n')
+
+# Aspas duplas
+print("Magno V Gomes")
+print(2, "Magno 'V Gomes'\n")
+
+# Escape
+print("Magno \"V Gomes\"\n")
+
+# r
+print(r"Magno \"V Gomes\"")
