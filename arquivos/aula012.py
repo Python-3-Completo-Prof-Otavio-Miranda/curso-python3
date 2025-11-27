6b5fb07a509bc87ab6820a4b5462a602958db8f5
Magno V Gomes <magnovgomes74@gmail.com>
2025-11-27 17:47:06 -0300

    Finalizando aula 012 - exercício op. aritméticos

---
+++ arquivos/aula012.py
@@ -0,0 +1,7 @@
+nome = 'Magno V Gomes'
+altura = 1.82
+peso = 156
+imc = peso / altura ** 2  # IMC = peso / (altura^2)
+
+print('Meu nome é', nome, ', tenho', altura, 'de altura,\n'
+      'peso', peso, 'quilos e meu IMC é:', imc, '\n')
