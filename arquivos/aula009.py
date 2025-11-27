48672de73d2686921d10bc34148cea82d5db78ff
Magno V Gomes <magnovgomes74@gmail.com>
2025-11-26 21:54:50 -0300

    Finalizando aula 009 - Introdução as operações aritméticas

---
+++ arquivos/aula009.py
@@ -0,0 +1,26 @@
+adicao = 110 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 3  # float - resultado exato
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 3  # resultado só com o nº inteiro
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10  # 2 elevado a décima potência
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2  # resto da divisão
+print('Módulo', modulo)
+
+print(10 % 8 == 0)  # para saber se um número é par ou ímpar
+print(16 % 8 == 0)  # se o resultado por 0 é par
+print(10 % 2 == 0)  # caso contrário será impar
+print(15 % 2 == 0)  # por enquanto par é True
+print(16 % 2 == 0)  # ímpar é False
