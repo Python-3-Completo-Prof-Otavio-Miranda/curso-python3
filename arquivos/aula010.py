63a589131779c201cdd1d1916a38adeafc201f77
Magno V Gomes <magnovgomes74@gmail.com>
2025-11-26 22:00:36 -0300

    Finalizando aula 010 - Concatenação (+) e repetição (*) com operadores aritméticos

---
+++ arquivos/aula010.py
@@ -0,0 +1,13 @@
+concatenacao1 = 'Magno' + ' ' + 'V Gomes'
+print(concatenacao1)  # união das strings com o uso do sinal '+'
+concatenacao2 = 'A' + 'B' + 'C'
+print(concatenacao2)  # se chama concatenação (string e '+')
+
+print()
+
+a_dez_vezes = 'A' * 10  # aqui já é string com inteiro
+tres_vezes_Magno = 3 * 'Magno'
+print(a_dez_vezes)  # resulta em repetição da string
+print(tres_vezes_Magno)
+
+print()
