fe6acb02a6bc2ed7d7bca3c4aff42d19fcc2e1d7
Magno V Gomes <magnovgomes74@gmail.com>
2025-12-01 14:53:10 -0300

    Finalizando aula 020 - exercício de fixação dos conteúdos estudados

---
+++ arquivos/aula020.py
@@ -0,0 +1,14 @@
+#  exercício de fixação dos conteúdos estudados
+
+primeiro_valor = input('Insira um valor: ')
+segundo_valor = input('Insira outro valor: ')
+
+
+if primeiro_valor > segundo_valor:
+    print(f'O {primeiro_valor=} é maior do que o {segundo_valor=}')
+
+elif primeiro_valor == segundo_valor:
+    print(f'O {primeiro_valor=} e o {segundo_valor=} são iguais.')
+
+else:
+    print(f'o {segundo_valor=} é maior do que o {primeiro_valor=}')
