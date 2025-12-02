e90e9334e95f66a2580647aa2926148397546574
Magno V Gomes <magnovgomes74@gmail.com>
2025-12-02 15:23:13 -0300

    Finalizando aula 023 - Operador Lógico 'not'

---
+++ arquivos/aula023.py
@@ -0,0 +1,24 @@
+# Introdução aos Operadores Lógicos -> and (e) or(ou) not(não)
+
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+# senha = input('Senha: ')
+
+# if senha != '123456':
+#     print('Senha incorreta.')
+
+# if not senha:
+#     print('Você não digitou nada!')
+
+# if senha == '123456':
+#     print('Entrou!')
+# else:
+#     print('Senha incorreta.')
+
+print(not True)  # -> False
+print(not False)  # -> True
+print(not 0)  # -> True
+print(not 1)  # -> False
